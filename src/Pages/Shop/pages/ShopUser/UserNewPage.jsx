60e3475e7ac8b64a3a21041d94c289980c91950c
codewithmahadihasan <codewithmahadihasan@gmail.com>
2024-01-09 07:49:48 +0600

    new deploy

--- src/Pages/Shop/pages/ShopUser/UserNewPage.jsx
+++ src/Pages/Shop/pages/ShopUser/UserNewPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const NewPage = () => {
+const UserNewPage = () => {
     return (
         <div>
             
@@ -8,4 +8,4 @@ const NewPage = () => {
     );
 };
 
-export default NewPage;
+export default UserNewPage;
