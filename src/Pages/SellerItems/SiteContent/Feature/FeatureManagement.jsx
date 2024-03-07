3aaf63bcd0c02cab4f7db09e3b8c4ae3af7fbda3
md nahid <mdnahid360s@gmail.com>
2024-03-07 19:57:54 +0600

    update

--- src/Pages/SellerItems/SiteContent/Feature/FeatureManagement.jsx
+++ src/Pages/SellerItems/SiteContent/Feature/FeatureManagement.jsx
@@ -12,7 +12,7 @@ import DeleteModal from '../../../../Common/DeleteModal';
 
 const FeatureManagement = () => {
     const [loading, setLoading] = useState(false);
-
+    const [openModal, setOpenModal] = useState(false);
     const { shopInfo } = useContext(AuthContext)
 
     const { data: faqs = [], refetch } = useQuery({
@@ -72,11 +72,57 @@ const FeatureManagement = () => {
 
 
 
-    const [OpenModal, setOpenModal] = useState(false)
 
-    const handleViewDetails = (ticketId) => {
-        setOpenModal(ticketId);
+    const [deleteOn, setDeleteOn] = useState(false)
+
+    const [selectIndex, setSelectIndex] = useState('')
+
+    const uploadImage = async (formData) => {
+        const url = `https://salenow-v2-backend.vercel.app/api/v1/image/upload-image`;
+        const response = await fetch(url, {
+            method: "POST",
+            body: formData,
+        });
+
+        const imageData = await response.json();
+        return imageData.imageUrl;
     };
+
+    const handleEditItem = async (e, id) => {
+        e.preventDefault();
+        const form = e.target;
+        const image = form.image;
+        const url = form.url.value;
+        const index = selectIndex;
+
+        const imageFormData = new FormData();
+        imageFormData.append("image", image.files[0]);
+        const imageUrl = await uploadImage(imageFormData);
+
+
+        const data = {
+            image: imageUrl ? imageUrl : openModal?.image,
+            link: url,
+            index
+        }
+
+        console.log(data, id);
+
+        fetch(`https://salenow-v2-backend.vercel.app/api/v1/seller/feature/update?id=${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        }).then((res) => res.json()).then((data) => {
+            Swal.fire(`image update`, '', 'success');
+            refetch()
+        })
+
+        setOpenModal(false);
+    }
+
+
 
     const [selectedImage, setSelectedImage] = useState(null);
 
@@ -91,7 +137,7 @@ const FeatureManagement = () => {
 
     return (
         <div>
-            <div className='h-0 w-0'>   <DeleteModal setOpenModal={setDeletePopUp} OpenModal={deletePopUp} setIsDelete={setIsDelete} /></div>
+            <div className='h-0 w-0'>   <DeleteModal setOpenModal={setDeleteOn} OpenModal={deletePopUp} setIsDelete={setIsDelete} /></div>
             <div className=''>
                 {selectedImage && (
                     <div className="fixed top-0 left-0 flex items-center justify-center w-full h-full bg-black bg-opacity-90 z-50">
@@ -151,6 +197,14 @@ const FeatureManagement = () => {
                                                     <span>Status</span>
                                                 </button>
                                             </th>
+                                            <th
+                                                scope="col"
+                                                className="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 "
+                                            >
+                                                <button className="flex items-center gap-x-2">
+                                                    <span>Index</span>
+                                                </button>
+                                            </th>
 
                                             <th
                                                 scope="col"
@@ -161,7 +215,7 @@ const FeatureManagement = () => {
                                         </tr>
                                     </thead>
                                     <tbody className="bg-white divide-y divide-gray-200 ">
-                                        {faqs?.map((faq, index) => (
+                                        {faqs?.sort((a, b) => parseInt(a.index) - parseInt(b.index))?.map((faq, index) => (
                                             <tr>
                                                 <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
                                                     <div className="inline-flex items-center gap-x-3">
@@ -209,6 +263,10 @@ const FeatureManagement = () => {
                                                 </td>
 
                                                 <td className="px-4 py-4 text-sm whitespace-nowrap">
+                                                    {faq?.index && faq?.index}
+                                                </td>
+
+                                                <td className="px-4 py-4 text-sm whitespace-nowrap">
                                                     <div className="flex items-center gap-x-6">
                                                         <button
                                                             onClick={() => DeleteSeller(faq?._id)}
@@ -230,12 +288,56 @@ const FeatureManagement = () => {
                                                             </svg>
                                                         </button>
 
-                                                        {/* <button onClick={() => handleViewDetails(faq?._id)}>
+                                                        <button onClick={() => setOpenModal(faq)}>
                                                             <BiEdit className=" transition-colors text-xl duration-200 text-yellow-500 hover:text-yellow-700 focus:outline-none" />
-                                                        </button> */}
+                                                        </button>
                                                     </div>
                                                 </td>
 
+
+                                                <div className={`fixed z-[100] flex items-center justify-center ${openModal?._id === faq?._id ? 'opacity-1 visible' : 'invisible opacity-0'} inset-0 bg-black/20 backdrop-blur-sm duration-100`}>
+                                                    <div className={`absolute md:w-[500px] w-full rounded-sm bg-white p-3 pb-5 text-center drop-shadow-2xl ${openModal?._id === faq?._id ? 'scale-1 opacity-1 duration-300' : 'scale-0 opacity-0 duration-150'} `}>
+                                                        <svg onClick={() => setOpenModal(false)} className="mx-auto mr-0 w-8 cursor-pointer" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g strokeWidth="0"></g><g strokeLinecap="round" strokeLinejoin="round"></g><g><path d="M6.99486 7.00636C6.60433 7.39689 6.60433 8.03005 6.99486 8.42058L10.58 12.0057L6.99486 15.5909C6.60433 15.9814 6.60433 16.6146 6.99486 17.0051C7.38538 17.3956 8.01855 17.3956 8.40907 17.0051L11.9942 13.4199L15.5794 17.0051C15.9699 17.3956 16.6031 17.3956 16.9936 17.0051C17.3841 16.6146 17.3841 15.9814 16.9936 15.5909L13.4084 12.0057L16.9936 8.42059C17.3841 8.03007 17.3841 7.3969 16.9936 7.00638C16.603 6.61585 15.9699 6.61585 15.5794 7.00638L11.9942 10.5915L8.40907 7.00636C8.01855 6.61584 7.38538 6.61584 6.99486 7.00636Z" fill="#000"></path></g></svg>
+
+                                                        <form onSubmit={(e) => handleEditItem(e, faq?._id)}>
+
+                                                            <h1 className="text-lg font-semibold text-center mb-4">
+                                                                Edit Feature Image
+                                                            </h1>
+
+                                                            <img src={faq?.image} alt="" className="w-[100px] h-[100px] rounded" />
+                                                            <div className="flex flex-col items-start gap-1">
+                                                                <label className='text-start' htmlFor="photo">Photo</label>
+                                                                <input
+
+                                                                    type="file" name="image" className='border border-gray-500 p-1 rounded mb-3 w-full' />
+                                                            </div>
+                                                            <div className="flex flex-col items-start gap-1">
+                                                                <label className='text-start' htmlFor="url">URL</label>
+                                                                <input
+                                                                    defaultValue={openModal?.link}
+                                                                    type="url" name="url" className='border border-gray-500 p-1 rounded mb-3 w-full' />
+                                                            </div>
+                                                            <div className="flex flex-col items-start gap-1">
+                                                                <label className='text-start' htmlFor="index">Index</label>
+                                                                <select
+                                                                    onChange={(e) => setSelectIndex(e.target.value)}
+                                                                    name="index" id="index"
+                                                                    className='border border-gray-500 p-1 rounded mb-3 w-full'>
+                                                                    <option value="1">1</option>
+                                                                    <option value="2">2</option>
+                                                                    <option value="3">3</option>
+                                                                    <option value="4">4</option>
+                                                                </select>
+                                                            </div>
+                                                            <br />
+                                                            <div className="flex justify-start">
+                                                                <button type='submit' className="me-2 rounded bg-green-700 px-6 py-1 text-white">Sibmit</button>
+                                                            </div>
+                                                        </form>
+
+                                                    </div>
+                                                </div>
                                             </tr>
                                         ))}
                                     </tbody>
