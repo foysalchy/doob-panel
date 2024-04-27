f6062ac17d9825fd1c8050fcc8ca61860f81f62f
sarwar-asik <sarwarasik@gmail.com>
2024-04-27 21:22:41 +0600

    admin/service: service edit updated

--- src/Pages/AdminItem/Services/EditService.jsx
+++ src/Pages/AdminItem/Services/EditService.jsx
@@ -1,11 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
 import JoditEditor from "jodit-react";
 import React, { useState } from "react";
 import { RxCross2 } from "react-icons/rx";
 import Swal from "sweetalert2";
+import UploadImage from "../../SellerItems/ProductManagement/SellerAddProduct/Components/UploadImage";
 
 const EditService = ({ OpenModal, setOpenModal, BlogInfo, refetch }) => {
   const [previewUrl, setPreviewUrl] = useState(BlogInfo.img);
 
+  const { data: categories = [] } = useQuery({
+    queryKey: ["category"],
+    queryFn: async () => {
+      const res = await fetch(
+        "https://backend.doob.com.bd/api/v1/admin/category"
+      );
+      const data = await res.json();
+      return data;
+    },
+  });
   const handleFileChange = async (event) => {
     const image = event.target.files[0];
     const formData = new FormData();
@@ -27,16 +39,46 @@ const EditService = ({ OpenModal, setOpenModal, BlogInfo, refetch }) => {
     }
   };
 
+  async function uploadImage(formData) {
+    const url = "https://backend.doob.com.bd/api/v1/image/upload-image";
+    const response = await fetch(url, {
+      method: "POST",
+      body: formData,
+    });
+    const imageData = await response.json();
+    return imageData.imageUrl;
+  }
+
   const handleBlogUpdate = async (e) => {
     e.preventDefault();
 
     const img = previewUrl;
     const title = e.target.title.value;
     const message = e.target.message.value;
+    const price = e.target.price.value;
     const MetaTag = e.target.MetaTag.value;
     const MetaDescription = e.target.MetaDescription.value;
+    const category = e.target.category.value;
+    const subscriptionPeriod = e.target.subscriptionPeriod.value;
+    const MetaImage = e.target.MetaImage.files[0];
 
-    const data = { img, title, message, MetaTag, MetaDescription };
+    const metaImageFormData = new FormData();
+    metaImageFormData.append("image", MetaImage);
+    console.log(metaImageFormData);
+
+    const metaImageUrl = await uploadImage(metaImageFormData);
+
+    const data = {
+      img,
+      title,
+      price,
+      message,
+      MetaTag,
+      MetaDescription,
+      category,
+      subscriptionPeriod,
+      MetaImage: metaImageUrl,
+    };
 
     try {
       const response = await fetch(
@@ -56,19 +98,22 @@ const EditService = ({ OpenModal, setOpenModal, BlogInfo, refetch }) => {
           setOpenModal(false);
         });
     } catch (error) {
-      console.error("Error updating blog:", error);
+      console.error("Error updating Service:", error);
     }
   };
 
+  console.log("BlogInfo?.category", BlogInfo?.category);
+
   return (
     <div
-      className={`fixed z-50 top-0 left-0 flex h-full min-h-screen w-full items-center justify-center bg-black bg-opacity-90 px-4 py-5 ${OpenModal ? "block" : "hidden"
-        }`}
+      className={`fixed z-50 top-0 left-0 flex h-full min-h-screen w-full items-center justify-center bg-black bg-opacity-90 px-4 py-5 ${
+        OpenModal ? "block" : "hidden"
+      }`}
     >
       <div className="w-full max-w-[800px]  rounded-[20px] bg-white pb-10 px-8 text-center md:px-[30px] ">
         <div className="flex justify-between z-50 pt-4 items-start w-full sticky top-0 bg-white border-b">
           <div className="pb-2 text-xl font-bold text-dark text-center sm:text-2xl">
-            Edit Blog
+            Edit Service
           </div>
           <div
             onClick={() => setOpenModal(!OpenModal)}
@@ -88,6 +133,65 @@ const EditService = ({ OpenModal, setOpenModal, BlogInfo, refetch }) => {
             defaultValue={BlogInfo.title}
           />
 
+          <div>
+            <label className="sr-only text-black" htmlFor="title">
+              Service Price
+            </label>
+            <input
+              required
+              className="w-full rounded-lg border border-gray-900 p-3 text-sm"
+              placeholder="Service Price"
+              type="text"
+              id="price"
+              defaultValue={BlogInfo?.price}
+              name="price"
+            />
+          </div>
+          <div className="relative mt-1.5">
+            <select
+              type="text"
+              id="Category"
+              defaultValue={{
+                label: BlogInfo?.category,
+                value: BlogInfo?.category,
+              }}
+              name="category"
+              className="w-full mt-1 rounded-lg border border-gray-900 px-3 py-2 text-sm"
+              placeholder="Select a category"
+            >
+              <option selected disabled value="Select Service Category">
+                Select Service Category
+              </option>
+              {categories.length &&
+                categories?.map((category, i) => (
+                  <option key={category?.title} value={category?.title}>
+                    {category?.title}
+                  </option>
+                ))}
+            </select>
+          </div>
+
+          <div className="relative mt-1.5">
+            <select
+              type="text"
+              list="subscriptionPeriod"
+              id="subscriptionPeriod"
+              name="subscriptionPeriod"
+              className="w-full mt-1 rounded-lg border border-gray-900 px-3 py-2 text-sm"
+              placeholder="Select Subscription Period"
+              defaultValue={{
+                label: BlogInfo?.subscriptionPeriod,
+                value: BlogInfo?.subscriptionPeriod,
+              }}
+            >
+              <option disabled selected className="" value="">
+                Select Service Parched Time
+              </option>
+              <option value="One Time">One Time</option>
+              <option value="Monthly">Monthly</option>
+              <option value="Yearly">Yearly</option>
+            </select>
+          </div>
           <div className="mb-4">
             <label
               htmlFor="dropzone-file"
@@ -122,6 +226,20 @@ const EditService = ({ OpenModal, setOpenModal, BlogInfo, refetch }) => {
             </div>
           </div>
 
+          <div>
+            <label className="sr-only text-black" htmlFor="title">
+              Meta Image'
+            </label>
+            <input
+              required
+              className="w-full rounded-lg border border-gray-900 p-3 text-sm"
+              placeholder="Meta Description"
+              type="file"
+              id="MetaImage"
+              name="MetaImage"
+            />
+          </div>
+
           <input
             name="MetaTag"
             className="w-full p-2 my-4 border"
@@ -135,7 +253,7 @@ const EditService = ({ OpenModal, setOpenModal, BlogInfo, refetch }) => {
             cols="20"
             rows="2"
           ></textarea>
-
+          {/* button */}
           <div className="flex justify-start">
             <button
               type="submit"
@@ -158,7 +276,7 @@ const EditService = ({ OpenModal, setOpenModal, BlogInfo, refetch }) => {
                 </svg>
               </span>
               <span className="text-sm font-medium transition-all group-hover:ms-4">
-                Update Blog
+                Update Service
               </span>
             </button>
           </div>
