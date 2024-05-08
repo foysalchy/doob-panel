1a781816b40fa657e66ff86a3076e2880345f005
nahid <mdnahid360s@gmail.com>
2024-05-08 10:31:35 -0700

    update

--- src/Pages/SellerItems/ReportManagement/PosHistory/PosInvoiceModal.jsx
+++ src/Pages/SellerItems/ReportManagement/PosHistory/PosInvoiceModal.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useContext, useRef } from 'react';
+import { AuthContext } from '../../../../AuthProvider/UserProvider';
+import { useReactToPrint } from 'react-to-print';
 
 const PosInvoiceModal = ({ setOpenInvoice, openInvoice, invoice }) => {
+    const { shopInfo } = useContext(AuthContext);
+    const componentRef = useRef();
+    const handlePrint = useReactToPrint({
+        content: () => componentRef.current,
+    });
     const style = {
         overlay: 'fixed top-0 left-0 right-0 bottom-0 w-full bg-black h-screen overflow-y-auto bg-white p-2 flex items-center justify-center z-[2000]',
         cart: 'bg-white',
         title: 'text-3xl font-semibold text-center pb-4',
         close: 'text-2xl p-2 absolute right-3 top-2'
     }
+
+    const invoiceData = invoice
     console.log(invoice.items);
     return (
         <div className={style.overlay}>
@@ -14,88 +23,94 @@ const PosInvoiceModal = ({ setOpenInvoice, openInvoice, invoice }) => {
                 <div className="inline-block  py-2 align-middle md:px-6 lg:px-8">
                     <div className="overflow-hidden max-w-3xl overflow-x-auto  border-gray-200 dark:border-gray-700 md:rounded-lg">
                         <h2 className={style?.title}>Products</h2>
-                        <button onClick={() => setOpenInvoice(!openInvoice)} className={style.close}>x</button>
-                        <table className=" divide-y divide-gray-200 dark:divide-gray-700">
-                            <thead className="bg-gray-50 dark:bg-gray-800">
-                                <tr>
-                                    <th
-                                        scope="col"
-                                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+ 
+                        <div className="fixed top-0 right-0 bottom-0 left-0 bg-black bg-opacity-50 flex items-center justify-center">
+                            <div className="bg-white rounded-md">
+                                <div className="flex p-4 justify-between">
+                                    <button
+                                        onClick={() => setOpenInvoice(!openInvoice)}
+                                        className="px-4 py-2 text-white rounded-md bg-red-600"
                                     >
-                                        Photo
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
-                                    >
-                                        Name
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
-                                    >
-                                        Name
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
-                                    >
-                                        Price
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
-                                    >
-                                        Quantity
-                                    </th>  <th
-                                        scope="col"
-                                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
-                                    >
-                                        SKU
-                                    </th>
+                                        Cancel
+                                    </button>
 
-                                </tr>
-                            </thead>
-                            <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
-                                {invoice.items.map(invoice =>
-                                    <tr key={invoice?._id}>
+                                    <button
+                                        onClick={handlePrint}
+                                        className="px-4 py-2 text-white rounded-md bg-blue-600"
+                                    >
+                                        Print
+                                    </button>
 
-                                        <td className="px-4 py-4 text-sm font-medium text-gray-700 dark:text-gray-200 whitespace-nowrap">
-                                            <div onClick={() => setOpenInvoice(itm?._id)} className="inline-flex items-center gap-x-3 cursor-pointer text-blue-500">
-                                                <img src={invoice.img} alt="" className="" />
+
+                                </div>
+                                <div id='invoice'
+                                    ref={componentRef}
+                                    className="w-full p-4 bg-white" style={{ width: '300px' }}>
+                                    <div className="flex  pb-2  border-b border-gray-700 text-start">
+                                        <div className="">
+                                            {shopInfo?.logo && <img src={shopInfo?.logo} className='' alt="" />}
+                                            <h3 className="text-lg font-semibold">Name: {shopInfo?.shopName}</h3>
+                                            <p className="text-xs text-gray-500">Email: {shopInfo?.shopEmail}</p>
+                                            <p className="text-xs text-gray-500">Phone: {shopInfo?.shopNumber}</p>
+                                            <p className="text-xs text-gray-500">Address: {shopInfo?.address}</p>
+                                        </div>
+                                        <div className='pl-2'>
+                                            <p className='text-xl text-blue-500'>Receipt</p>
+                                            <p className="text-sm">Paid Date: {new Date().toLocaleDateString()}</p>
+                                        </div>
+                                    </div>
+                                    <ul className=''>
+                                        <li className="py-2 border-b border-gray-700">
+                                            <div className="flex justify-between">
+                                                <h2 className="text-sm font-semibold">Product Name</h2>
+                                                <h2 className="text-sm font-semibold">Price</h2>
                                             </div>
-                                        </td>
-                                        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-wrap">
-                                            {invoice.name}
-                                        </td>
-                                        <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                                            <div className="text-emerald-500">
-                                                <h2 className="text-sm font-normal">
-                                                    {invoice.price}
-                                                </h2>
+                                        </li>
+                                        {
+                                            invoiceData?.items?.map((itm, index) => (
+                                                <li key={index} className="py-2 border-b border-gray-700">
+                                                    <div className="flex justify-between items-start">
+                                                        <span>{`(${index + 1})`}</span>
+                                                        <h2 className="w-[150px] text-sm">{itm?.name}</h2>
+                                                        <div className="">
+                                                            <h2 className="text-sm">৳ {itm?.price}</h2>
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                            ))
+                                        }
+                                    </ul>
+                                    <ul className='mt-4'>
+                                        <li className="py-2 border-b border-gray-700">
+                                            <div className="flex justify-between">
+                                                <h2 className="text-sm font-semibold">Total:</h2>
+                                                <h2 className="text-sm">৳ {invoiceData?.total}</h2>
                                             </div>
-                                        </td>
-                                        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-                                            <div className="flex items-center gap-x-2">
-                                                <div>
-                                                    <h2 className="text-sm  text-gray-800 dark:text-white ">
-                                                        {invoice.quantity}
-                                                    </h2>
-
-                                                </div>
+                                        </li>
+                                        <li className="py-2 border-b border-gray-700">
+                                            <div className="flex justify-between">
+                                                <h2 className="text-sm font-semibold">Pay Amount:</h2>
+                                                <h2 className="text-sm">৳ {invoiceData?.cash}</h2>
                                             </div>
-                                        </td>
+                                        </li>
+                                        <li className="py-2 border-b border-gray-700">
+                                            <div className="flex justify-between">
+                                                <h2 className="text-sm font-semibold">Payment Getaway:</h2>
+                                                <h2 className="text-sm">{invoiceData?.invoice?.
+                                                    getaway
+                                                }</h2>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                    <div className="flex justify-between mt-4">
+                                        <h2 className="text-sm font-semibold">Change:</h2>
+                                        <h2 className="text-sm">৳ {invoiceData?.change}</h2>
+                                    </div>
 
-                                        <td className="px-4 py-4 text-sm whitespace-nowrap">
-                                            <h2 className="text-sm  text-gray-800 dark:text-white ">
-                                                {invoice.sku}
-                                            </h2>
+                                </div>
+                            </div >
+                        </div >
 
-                                        </td>
-
-                                    </tr>)}
-                            </tbody>
-                        </table>
                     </div>
                 </div>
             </div>
--- src/Pages/Shop/pages/ConfirmOrder/ServiceConfirmOrder.jsx
+++ src/Pages/Shop/pages/ConfirmOrder/ServiceConfirmOrder.jsx
@@ -1,9 +1,11 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState, useContext } from 'react';
 import vct from '../../../../assets/vct.png';
 import { useReactToPrint } from 'react-to-print';
+import { AuthContext } from '../../../../AuthProvider/UserProvider';
 
 const ServiceConfirmOrder = () => {
     const componentRef = useRef();
+    const { shopInfo, user } = useContext(AuthContext)
     const handlePrint = useReactToPrint({
         content: () => componentRef.current,
     });
@@ -27,13 +29,129 @@ const ServiceConfirmOrder = () => {
     const order = JSON.parse(data);
 
 
+    const InvoicePage = ({ itm }) => {
+        return (
+            <>
+                <div
+                    ref={componentRef}
+                    className="px-12 bg-white pb-12 pt-2 mx-8 print-data">
+
+                    <header className="flex items-start justify-between">
+                        {/* <img src={shopInfo?.logo} alt="logo" className='w-[200px]' /> */}
+                        <div className='whitespace-wrap w-[300px]'>
+                            {/* <p className='text-gray-600 text-end'>{shopInfo?.address}</p>
+              <p className='text-gray-600 text-end'>{shopInfo?.shopName}</p> */}
+                        </div>
+                    </header>
+
+                    <main>
+                        <div className="flex items-center justify-center py-1 font-bold text-gray-600 bg-gray-200 mt-8 text-center ">
+                            INVOICE
+                        </div>
+
+                        {/*................*/}
+                        {/*.... Address ...*/}
+                        {/*................*/}
+                        <div className="hidden items-start justify-between mt-4">
+                            <div>
+                                <div className='flex items-center gap-2'>
+                                    <h4 className='font-semibold text-gray-700 text-sm'>
+                                        Email :
+                                    </h4>
+                                    <p className="text-gray-600 text-sm">{user?.email}</p>
+                                </div>
+                                <div className='flex items-center gap-2'>
+                                    <h4 className='font-semibold text-gray-700 text-sm'>
+                                        Phone :
+                                    </h4>
+                                    <p className="text-gray-600 text-sm">{user?.phoneNumber}</p>
+                                </div>
+                            </div>
+
+                            <div>
+                                <li className='flex justify-start items-center gap-2'>
+                                    <h4 className='font-semibold text-gray-700 text-sm'>
+                                        Invoice No : {user?._id}
+                                    </h4>
+                                    {/* <p className="text-gray-600 text-sm">{shopInfo?._id}</p> */}
+                                </li>
+
+                            </div>
+
+                        </div>
+
+                        {/*................*/}
+                        {/*.... Product ...*/}
+                        {/*................*/}
+
+                        <section className="container mx-auto mt-8">
+                            <div className="w-full mb-8 overflow-hidden">
+                                <div className="w-full overflow-x-auto border">
+                                    <table className="w-full">
+                                        <thead>
+                                            <tr className="text-md font-semibold tracking-wide text-left text-gray-100 bg-gray-900 uppercase border-b border-gray-900">
+                                                <th className="px-4 py-2">Photo</th>
+                                                <th className="px-4 py-2">Name</th>
+                                                <th className="px-4 py-2 whitespace-nowrap">Stock Quantity</th>
+                                                <th className="px-4 py-2">Price</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody className="bg-white">
+                                            {itm?.map(itm => (
+                                                <tr className='border-t' key={itm?._id}>
+                                                    <td className="p-4 w-[110px] border-b border-blue-gray-50">
+                                                        <img src={itm?.img} alt="" className="w-[100px] object-cover h-[80px] rounded border" />
+                                                    </td>
+                                                    <td className="p-4 border-b w-[300px] border-blue-gray-50">
+                                                        {itm?.title}
+                                                    </td>
+                                                    <td className="p-4 border-b border-blue-gray-50">
+                                                        {itm?.price}
+                                                    </td>
+                                                    <td className="p-4 border-b border-blue-gray-50">
+                                                        1  {/* {itm?.quantity} */}
+                                                    </td>
+                                                </tr>
+                                            ))}
+
+                                            {/* <tr>
+                                                <td colSpan={6} className='px-1 py-2 text-sm border  border-gray-800'></td>
+                                                <td colSpan={1} className='px-1 py-2 text-sm border-b  border-gray-800 text-end'>
+                                                    TOTAL:
+                                                </td>
+                                                <td colSpan={1} className='px-1 py-2 text-sm border  border-gray-800 text-start'>
+                                                    $5000
+                                                </td>
+                                            </tr> */}
+                                            {/* Add more rows here */}
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </section>
+
+
+
+
+                    </main>
+                    <footer>
+
+                    </footer>
+                </div>
+            </>
+        )
+    }
+
     return (
-        <div className=''>
-            <div ref={componentRef} className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+        <div className='bg-gray-50'>
+            <div ref={componentRef} className="px-4 py-5 mx-auto bg-gray-50 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
                 <h1 className="text-center text-3xl mt-12 font-bold">Your order is confirmed</h1>
                 <p className="text-center text-gray-500">Thank you for shopping.</p>
 
-                <div className="grid grid-cols-3 gap-3">
+                <button onClick={handlePrint} className='bg-blue-500 mb-3 text-white px-6 py-2 rounded-md mt-2'>Print</button>
+                <InvoicePage itm={order} />
+
+                <div className="hidden grid-cols-3 gap-3">
                     <div ref={componentRef} className="col-span-2">
                         <>
                             <div className="p-6 overflow-x-auto px-0">
