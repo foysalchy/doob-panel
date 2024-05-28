75f088a7ed6fbe29d6deb129cf5d99fcfb3ef198
sarwar-asik <sarwarasik@gmail.com>
2024-05-28 23:10:17 +0600

    admin (blogs) : updated admin blogs

--- src/Pages/AdminItem/Blogs/EditBlog.jsx
+++ src/Pages/AdminItem/Blogs/EditBlog.jsx
@@ -1,11 +1,12 @@
 import JoditEditor from "jodit-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { RxCross2 } from "react-icons/rx";
 import Swal from "sweetalert2";
 
 const EditBlog = ({ OpenModal, setOpenModal, BlogInfo, refetch }) => {
   const [previewUrl, setPreviewUrl] = useState(BlogInfo.img);
 
+  console.log(BlogInfo);
   const modules = {
     toolbar: [
       [{ header: "1" }, { header: "2" }, { font: [] }],
@@ -41,7 +42,36 @@ const EditBlog = ({ OpenModal, setOpenModal, BlogInfo, refetch }) => {
       console.error("Error uploading image:", error);
     }
   };
+  const [upload, setUpload] = useState(BlogInfo?.MetaImage ?? "");
+  const [uplodOk, setUploadOk] = useState(false);
 
+  const imageUploading = (e) => {
+    e.preventDefault();
+    const selectedFile = e.target.files[0];
+    const formData = new FormData();
+    formData.append("image", selectedFile);
+    const url = `https://backend.doob.com.bd/api/v1/image/upload-image`;
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((imageData) => {
+        console.log(imageData);
+        if (imageData.imageUrl) {
+          setUpload(imageData.imageUrl);
+          // setFormData({ ...formData, MetaImage: imageData.imageUrl });
+          setUploadOk(true);
+        } else {
+          setUpload("");
+        }
+      });
+  };
+
+  // useEffect(() => {
+  //   setUpload(BlogInfo?.MetaImage);
+  // },[BlogInfo?.MetaImage]);
+  console.log(upload);
   const handleBlogUpdate = async (e) => {
     e.preventDefault();
 
@@ -51,8 +81,16 @@ const EditBlog = ({ OpenModal, setOpenModal, BlogInfo, refetch }) => {
     const MetaTag = e.target.MetaTag.value;
     const MetaDescription = e.target.MetaDescription.value;
 
-    const data = { img, title, message, MetaTag, MetaDescription };
+    const data = {
+      img,
+      title,
+      message,
+      MetaTag,
+      MetaDescription,
+      MetaImage: upload,
+    };
 
+    // console.log(data);
     try {
       const response = await fetch(
         `https://backend.doob.com.bd/api/v1/admin/blog/update-blog/${BlogInfo._id}`,
@@ -77,8 +115,9 @@ const EditBlog = ({ OpenModal, setOpenModal, BlogInfo, refetch }) => {
 
   return (
     <div
-      className={`fixed z-50 top-0 left-0 flex h-full min-h-screen w-full items-center justify-center bg-black bg-opacity-90 px-4 py-5 ${OpenModal ? "block" : "hidden"
-        }`}
+      className={`fixed z-50 top-0 left-0 flex h-full min-h-screen w-full items-center justify-center bg-black bg-opacity-90 px-4 py-5 ${
+        OpenModal ? "block" : "hidden"
+      }`}
     >
       <div className="w-full max-w-[800px]  rounded-[20px] bg-white pb-10 px-8 text-center md:px-[30px]">
         <div className="flex justify-between z-50 pt-4 items-start w-full sticky top-0 bg-white border-b">
@@ -130,7 +169,8 @@ const EditBlog = ({ OpenModal, setOpenModal, BlogInfo, refetch }) => {
 
           <div>
             <div>
-              <JoditEditor id="message "
+              <JoditEditor
+                id="message "
                 name="message"
                 value={BlogInfo.message}
                 config={{
@@ -138,7 +178,8 @@ const EditBlog = ({ OpenModal, setOpenModal, BlogInfo, refetch }) => {
                   uploader: {
                     insertImageAsBase64URI: true,
                   },
-                }} />
+                }}
+              />
               {/* <ReactQuill
                 name="message"
                 id="message"
@@ -147,16 +188,18 @@ const EditBlog = ({ OpenModal, setOpenModal, BlogInfo, refetch }) => {
                 modules={modules}
                 placeholder="Enter description here..."
               /> */}
-           
+
               {/* <JoditEditor value={BlogInfo.message} /> */}
             </div>
           </div>
 
-          <input
-            name="MetaTag"
-            className="w-full p-2 my-4 border"
-            defaultValue={BlogInfo.MetaTag}
-          />
+          <div>
+            <input
+              name="MetaTag"
+              className="w-full p-2 my-4 border"
+              defaultValue={BlogInfo.MetaTag}
+            />
+          </div>
           <textarea
             className="w-full border p-2"
             defaultValue={BlogInfo.MetaDescription}
@@ -165,6 +208,39 @@ const EditBlog = ({ OpenModal, setOpenModal, BlogInfo, refetch }) => {
             cols="20"
             rows="2"
           ></textarea>
+          <label className="sr-only text-black" htmlFor="MetaImage">
+            Meta Image
+          </label>
+          <div>
+            <label
+              htmlFor="dropzone-file"
+              className="flex flex-col items-center w-full p-5 mx-auto mt-2 text-center bg-white border-2 border-gray-300 border-dashed cursor-pointer rounded-xl"
+            >
+              <img
+                srcSet={upload}
+                src={upload || BlogInfo.upload}
+                alt="File Preview"
+                className="mt-2 w-8 h-8"
+              />
+              <h2 className="mt-1 font-medium tracking-wide text-gray-700 ">
+                {upload.slice(25)}
+              </h2>
+            </label>
+            <input
+              onChange={imageUploading}
+              // required
+              className="w-full rounded-lg border border-gray-900 p-3 text-sm"
+              placeholder="Meta Description"
+              type="file"
+              id="MetaImage'"
+              name="MetaImage'"
+              // defaultValue={
+              //   restoreDrafts && draftsBlogData?.MetaImage
+              //     ? draftsBlogData?.MetaImage
+              //     : ""
+              // }
+            />
+          </div>
 
           <div className="flex justify-start">
             <button
