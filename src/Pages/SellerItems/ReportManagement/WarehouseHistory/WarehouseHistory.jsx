363d2fd890cf7758826b643fd8d9926752c0d9f7
nahid <mdnahid360s@gmail.com>
2024-06-06 16:32:34 +0600

    fix pagenaton issue in warehouse seller

--- src/Pages/SellerItems/ReportManagement/WarehouseHistory/WarehouseHistory.jsx
+++ src/Pages/SellerItems/ReportManagement/WarehouseHistory/WarehouseHistory.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../../AuthProvider/UserProvider";
 import { useQuery } from "@tanstack/react-query";
@@ -22,7 +22,6 @@ const WarehouseHistory = () => {
     },
   });
 
-  console.log(warehouseData);
   const { data: wareLength = [], refetch: reload } = useQuery({
     queryKey: ["wareLengthData"],
     queryFn: async () => {
@@ -34,22 +33,28 @@ const WarehouseHistory = () => {
     },
   });
 
-  console.log(wareLength);
 
   // Calculate total number of pages
   const totalPages = Math.ceil(warehouseData.length / itemsPerPage);
 
   // Function to paginate data
   const paginate = (pageNumber) => {
-    return warehouseData.slice(
+    const paginatedData = warehouseData.slice(
       (pageNumber - 1) * itemsPerPage,
       pageNumber * itemsPerPage
     );
+    console.log(paginatedData);
+    return paginatedData;
   };
 
   const [currentPage, setCurrentPage] = useState(1);
+  const paginatedData = paginate(currentPage);
 
   const [isPreviewModal, setIsPreviewModal] = useState(false);
+
+  useEffect(() => {
+    paginate(currentPage);
+  }, [currentPage, warehouseData]);
 
   return (
     <div>
@@ -118,7 +123,7 @@ const WarehouseHistory = () => {
                     </tr>
                   </thead>
                   <tbody className="">
-                    {warehouseData.map((data) => (
+                    {paginatedData.map((data) => (
                       <tr key={data?.warehouse?._id}>
                         <td className="px-4 py-4 text-sm font-medium text-gray-700 dark:text-gray- whitespace-nowrap">
                           <img
@@ -257,7 +262,7 @@ const WarehouseHistory = () => {
             <a
               href="#"
               onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
-              className="flex items-center px-2 py-2 mr-2 text-sm  capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 text-white dark:text-gray text-white- dark:border-gray-700 dark:hover:bg-gray-800"
+              className="flex items-center px-2 py-2 mr-2 text-sm capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 text-white dark:text-gray dark:border-gray-700 dark:hover:bg-gray-800"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -280,11 +285,10 @@ const WarehouseHistory = () => {
                   key={i}
                   href="#"
                   onClick={() => setCurrentPage(i + 1)}
-                  className={`px-2 py-1 mr-2 text-sm rounded-md ${
-                    currentPage === i + 1
-                      ? "text-blue-500 dark:bg-gray-800 bg-blue-100/60"
-                      : "text-gray-200 dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
-                  }`}
+                  className={`px-2 py-1 mr-2 text-sm rounded-md ${currentPage === i + 1
+                    ? "text-blue-500 dark:bg-gray-800 bg-blue-100/60"
+                    : "text-gray-200 dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
+                    }`}
                 >
                   {i + 1}
                 </a>
@@ -295,7 +299,7 @@ const WarehouseHistory = () => {
               onClick={() =>
                 setCurrentPage((prev) => Math.min(prev + 1, totalPages))
               }
-              className="flex items-center  px-2 py-2 text-sm text-gray-200 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray- dark:border-gray-700 dark:hover:bg-gray-800"
+              className="flex items-center px-2 py-2 text-sm text-gray-200 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:border-gray-700 dark:hover:bg-gray-800"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
