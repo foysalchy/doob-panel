8bc947632fb1a8109c2e67257e8baad5873ac719
sarwar-asik <sarwarasik@gmail.com>
2024-06-23 23:02:36 +0600

    pos(seller) : fixed fos error

--- src/Pages/Shop/pages/Home/UserProfile/ProfileUpdate/EditAddress.jsx
+++ src/Pages/Shop/pages/Home/UserProfile/ProfileUpdate/EditAddress.jsx
@@ -111,11 +111,11 @@ const EditAddress = ({
       });
   };
 
-  const upazilla = district ? JSON?.parse(district)?.upazilla : [];
+  const upazilla = district ? JSON?.     parse(district)?.upazilla : [];
 
   return (
     <div>
-      <form onSubmit={submitData} className="text-start w-full z-50">
+      <form onSubmit={submitData} class                                  Name="text-start w-full z-50">
         <div className="mt-4 w-full">
           <label
             htmlFor="fullName"
