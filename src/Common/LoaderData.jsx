edce553abf6bff345e58379c80a52de19f4e31d3
Foysal Mahmud <foysal.chy2801@gmail.com>
2024-08-13 00:52:52 +0600

    loaderchange

--- src/Common/LoaderData.jsx
+++ src/Common/LoaderData.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 export default function LoaderData() {
   return (
-    <div>
+    <div className="load">
+      <style jsx>{`
+       .load{
+       text-align: center;
+border: 1px solid;
+border-radius: 5px;
+       }
+      `}</style>
       <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
     </div>
 
